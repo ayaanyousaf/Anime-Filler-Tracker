fe86e6a385ce8bb450d3709fc71ccc176283fc63
ayaan.ysf4 <ayaanyousaf@users.noreply.github.com>
2024-01-11 04:27:46 -0500

    Fixed error handling, added label text wrapping, and previous results now clear when search is pressed

--- filler_gui.py
+++ filler_gui.py
@@ -1,5 +1,5 @@
 import customtkinter as ctk 
-from filler_tracker import get_filler_episodes, get_filler_percentage, format_name
+from filler_tracker import get_filler_episodes, get_filler_percentage, find_url
 
 def search(): 
     """
@@ -8,19 +8,34 @@ def search():
         to display all info.
     """
     anime_name = entry.get()
-    url = f'https://www.animefillerlist.com/shows/{format_name(anime_name)}'
+
+    # Clear previous results
+    heading_label.configure(text='')
+    output_label.configure(text='')
 
     try: 
-        episodes = get_filler_episodes(anime_name, url)
-        percentage = get_filler_percentage(anime_name, url)
+        url, matched_name = find_url(anime_name)
+        episodes = get_filler_episodes(matched_name, url)
+        percentage = get_filler_percentage(matched_name, url)
 
         # Configure heading and output labels
-        heading_label.configure(text=f"ALL FILLER EPISODES FOR {anime_name.upper()}")
+        heading_label.configure(text=f"All Filler Episodes for: {matched_name}")
         output_label.configure(text=f"{episodes}\n{percentage}")
 
-    except: 
+    except Exception as e: 
         heading_label.configure(text="ERROR")
-        output_label.configure(text=f"Show {anime_name} not found. Please try a different name.")
+        print(str(e))
+        
+        if "Invalid URL" in str(e): 
+            error = f"Show {anime_name} not found. Please try a different name."
+        elif "output" in str(e): 
+            error = "Unable to retrieve filler episodes."
+        elif "percentage" in str(e): 
+            error = "Unable to retrieve filler percentage."
+        else: 
+            error = "Unknown Error"
+        
+        output_label.configure(text=error)
 
 # Setup 
 ctk.set_appearance_mode("dark")
@@ -48,10 +63,12 @@ button = ctk.CTkButton(master=frame, text="Search", command=search)
 button.pack(pady=12, padx=10)
 
 # Output Labels
-heading_label = ctk.CTkLabel(master=frame, text="", font=("Roboto", 18), anchor="center", justify="center")
+heading_label = ctk.CTkLabel(master=frame, text="", font=("Roboto", 18),\
+                             anchor="center", justify="center", wraplength=600)
 heading_label.pack(pady=12, padx=10)
 
-output_label = ctk.CTkLabel(master=frame, text="", font=("Roboto", 14), anchor="center", justify="left")
+output_label = ctk.CTkLabel(master=frame, text="", font=("Roboto", 14),\
+                            anchor="center", justify="left", wraplength=600)
 output_label.pack(pady=12, padx=10)
 
 root.mainloop()
