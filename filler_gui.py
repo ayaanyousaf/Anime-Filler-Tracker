40cfb5d4de0ec5a9fb36c60a56de579d626aeec1
ayaan.ysf4 <ayaanyousaf@users.noreply.github.com>
2024-01-11 21:13:50 -0500

    Revamped GUI, fixed tests, added custom logo

--- filler_gui.py
+++ filler_gui.py
@@ -1,12 +1,9 @@
 import customtkinter as ctk 
+from PIL import Image
 from filler_tracker import get_filler_episodes, get_filler_percentage, find_url
+from tkinter import font
 
 def search(): 
-    """
-        Search function that executes when the search button is pressed.
-        Receives user input from entry then uses functions from filler_tracker.py
-        to display all info.
-    """
     anime_name = entry.get()
 
     # Clear previous results
@@ -19,11 +16,14 @@ def search():
         percentage = get_filler_percentage(matched_name, url)
 
         # Configure heading and output labels
-        heading_label.configure(text=f"All Filler Episodes for: {matched_name}")
-        output_label.configure(text=f"{episodes}\n{percentage}")
+        heading_label.configure(text=f"All Filler Episodes for: {matched_name}", 
+                    font=ctk.CTkFont(family='Viner Hand ITC', weight='bold', size=20))
+        output_label.configure(text=f"{episodes}\n{percentage}",
+                    font=ctk.CTkFont(family='Roboto', weight='bold', slant='italic', size=14))
 
     except Exception as e: 
-        heading_label.configure(text="ERROR")
+        heading_label.configure(text="ERROR", font=('Roboto', 14))
+
         print(str(e))
         
         if "Invalid URL" in str(e): 
@@ -35,40 +35,53 @@ def search():
         else: 
             error = "Unknown Error"
         
-        output_label.configure(text=error)
+        output_label.configure(text=error, font=('Roboto', 12))
 
-# Setup 
+# Window Setup
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("dark-blue")
 
 root = ctk.CTk()
 root.geometry("800x550")
 root.title("Anime Filler Tracker")
-root.iconbitmap("assets/app_icon.ico")
+root.iconbitmap("assets/icon.ico")
 
-# Frame
-frame = ctk.CTkScrollableFrame(master=root)
-frame.pack(pady=20, padx=60, fill="both", expand=True)
+# Scrollable Frame
+frame = ctk.CTkScrollableFrame(master=root, scrollbar_button_color='white', scrollbar_button_hover_color='grey')
+frame.pack(padx=(10, 10), pady=(15, 15), fill="both", expand=True)
 
-# Title Label 
-title_label = ctk.CTkLabel(master=frame, text="Anime Filler Tracker", font=("Roboto", 20, "bold"))
-title_label.pack(pady=12, padx=10)
+# Logo
+logo_img = ctk.CTkImage(dark_image=Image.open('assets/logo.png'), size=(300, 300))
+logo_label = ctk.CTkLabel(master=frame, text="", image=logo_img)
+logo_label.pack(padx=30)
 
-# Entry
-entry = ctk.CTkEntry(master=frame, placeholder_text="Enter anime")
-entry.pack(pady=12, padx=10)
+# Search Bar
+entry = ctk.CTkEntry(master=frame, placeholder_text="Enter an anime", width=270)
+entry.pack(padx=10)
 
-# Button
-button = ctk.CTkButton(master=frame, text="Search", command=search)
-button.pack(pady=12, padx=10)
+# Search Button
+button = ctk.CTkButton(master=frame, 
+                    text="Search", 
+                    font=('Roboto', 14), 
+                    command=search,
+                    fg_color='white', 
+                    text_color='black', 
+                    hover_color='grey')
+button.pack(padx=10, pady=10)
 
 # Output Labels
-heading_label = ctk.CTkLabel(master=frame, text="", font=("Roboto", 18),\
-                             anchor="center", justify="center", wraplength=600)
-heading_label.pack(pady=12, padx=10)
+heading_label = ctk.CTkLabel(master=frame, 
+                    text="", 
+                    anchor="center", 
+                    justify="center", 
+                    wraplength=600)
+heading_label.pack(padx=6, pady=10)
 
-output_label = ctk.CTkLabel(master=frame, text="", font=("Roboto", 14),\
-                            anchor="center", justify="left", wraplength=600)
-output_label.pack(pady=12, padx=10)
+output_label = ctk.CTkLabel(master=frame, 
+                    text="", 
+                    anchor="center", 
+                    justify="left", 
+                    wraplength=700)
+output_label.pack(pady=10)
 
 root.mainloop()
--- test.py
+++ test.py
@@ -1,17 +1,17 @@
 import unittest
-from filler_tracker import get_filler_episodes, get_filler_percentage, format_name
+from filler_tracker import get_filler_episodes, get_filler_percentage, find_url
 
 class TestFillerTrackerFunctions(unittest.TestCase):
     def test_get_filler_episodes(self):
         anime_name = "Naruto"
         anime_url = "https://animefillerlist.com/shows/naruto"
         result = get_filler_episodes(anime_name, anime_url)
-        self.assertTrue(result)
+        self.assertIn("26 - ", result)
 
-    def test_format_name(self):
-        anime_name = "Attack on Titan"
-        result = format_name(anime_name)
-        self.assertEqual(result, "attack-titan")
+    def test_find_url(self):
+        anime_name = "Jujuts kaisen"
+        result = find_url(anime_name)
+        self.assertEqual(("https://animefillerlist.com/shows/jujutsu-kaisen", "Jujutsu Kaisen "), result)
 
     def test_get_filler_percentage(self):
         anime_name = "One Piece"
